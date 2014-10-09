f392be5640d99a945faaaafeb82bd17110e7d1b7
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2014-10-08 21:40:52 -0400

    finish semaine_7

--- semaine_7/src/semaine_7/String_1.java
+++ semaine_7/src/semaine_7/String_1.java
@@ -1,46 +1,30 @@
 package semaine_7;
 
-import javax.swing.JOptionPane;
-
 public class String_1 {
 	final static int TAILLE = 9;
 	
 	public static void main(String[] args) {
-		final int NOMBRE =  500;
+		final int NOMBRE =  5;
 		
-		String numero[] = new String[NOMBRE];
-		String produit;
-		int somme;
-		int i, j;
-		int tmp;
+		String numero;
+		int i;
 		
-		for(i = 0; i < NOMBRE; i++) {
-			numero[i] = "";
-			produit   = "";
-			somme     = 0;
-			
-			for(j = 0; j < TAILLE; j++) {
-				if(j == TAILLE-1) {
-					tmp = (sumString(""+(Integer.parseInt(produit)*2))+somme)%10;
-					if(tmp != 0)
-						tmp = 10-tmp;
-					else
-						tmp = 0;
-				} else if(j%2 == 0) {
-					tmp = generate(0, 9);
-					somme += tmp;
-				} else {
-					tmp = generate(0, 9);
-					produit += tmp;			
-				}
-				
-				numero[i] += tmp;
-			}
-			
-			System.out.print(numero[i]+"->"+(isNumero(numero[i])==true?"valide":"non-valide")+"\n");
+		for(i = 0; i < NOMBRE; i++) {	
+			numero = genNumero();
+			System.out.print(numero+"->"+(isNumero(numero)==true?"valide":"non-valide")+"\n");
 		}
 		
 		System.exit(0);
+	}
+
+	private static int sumString(String numero) {
+		int i;
+		int sum = 0;
+		
+		for(i = 0; i < numero.length(); i++)
+			sum += Character.getNumericValue(numero.charAt(i));
+
+		return sum;
 	}
 	
 	private static boolean isNumero(String numero) {
@@ -62,14 +46,37 @@ public class String_1 {
 		return ((produit+somme)%10==0?true:false);
 	}
 	
-	private static int sumString(String numero) {
+	private static String genNumero() {
+		String numero;
+		String produit;
+		int somme;
+		int tmp;
 		int i;
-		int sum = 0;
 		
-		for(i = 0; i < numero.length(); i++)
-			sum += Character.getNumericValue(numero.charAt(i));
-
-		return sum;
+		
+		numero  = "";
+		produit = "";
+		somme   = 0;
+		
+		for(i = 0; i < TAILLE; i++) {
+			if(i == TAILLE-1) {
+				tmp = (sumString(""+(Integer.parseInt(produit)*2))+somme)%10;
+				if(tmp != 0)
+					tmp = 10-tmp;
+				else
+					tmp = 0;
+			} else if(i%2 == 0) {
+				tmp = generate(0, 9);
+				somme += tmp;
+			} else {
+				tmp = generate(0, 9);
+				produit += tmp;			
+			}
+			
+			numero += tmp;
+		}
+		
+		return numero;
 	}
 	
 	private static int generate(int min, int max) {
