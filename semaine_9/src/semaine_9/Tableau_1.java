5514443c50ddde6c9661c0206d742977a916a282
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2014-11-15 09:59:27 -0500

    Replace tabs by spaces

--- semaine_9/src/semaine_9/Tableau_1.java
+++ semaine_9/src/semaine_9/Tableau_1.java
@@ -1,8 +1,10 @@
 package semaine_9;
 
 import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+import java.text.DecimalFormat;
 
-/* 
+/*
  * Fichier     : Tableau_1.java
  * Project     : Laboratoires sur les tableaux
  * Objectifs   : Gestion du nombre de journées de maladie accumulées du personel
@@ -13,90 +15,154 @@ import javax.swing.JOptionPane;
  */
 
 public class Tableau_1 {
-	public static void main(String[] args) {
-		final int PSUPNB  = 10; // nombre de journée précis supérieur
-		final int EXACTNB =  8; // nombre de journée précis
-		
-		// tableau contenant les informations des employés
-		String nom[] = {"Benoit", "Alice", "Daniel", "Emile", "Julien"};
-		int numero[] = { 20,       10,      40,       50,      80     };
-		int malade[] = { 18,        8,      10,       11,       8     };
-		
-		int i;               // contient un compteur
-		int taille;          // contient le nombre d'employé
-		int sommeP      = 0; // contient la somme des journées des employés avec plus de N jours
-		int min         = 0; // contient l'index de l'employé avec le moins de jours
-		int max         = 0; // contient l'index de l'employé avec le plus de jours
-		int maladeE     = 0; // contient l'index de l'employé avec un nombre N de jours
-		int nombre;          // contient un nombre entré par l'utilisateur
-		int index       = 0; // contient l'index de l'employé demandé
-		double moyenne  = 0; // contient la moyenne de jours de tous les employés
-		double moyenneP = 0; // contient la moyenne de jours des employés avec plus de N jours
+    final static String NOM = "Gestionnaire de journées de maladie";
+    final static String ERR  = "Annuler";
 
-		taille = nom.length;
-		
-		for(i = 0; i < taille; i++) {
-			// somme des journées des employés avec plus de N jours
-			if(malade[i] >= PSUPNB)
-				sommeP++;
-			
-			// somme de la moyenne des journées des employés avec plus de N jours
-			if(malade[i] < PSUPNB)
-				moyenneP += malade[i]; 
-			
-			// somme de la moyenne des journées de tous les employées
-			moyenne += malade[i];
-			
-			// trouver l'index de l'employé avec le moins de jours
-			if(malade[i] < malade[min])
-				min = i;
+    public static void main(String[] args) {
+        final String menu[] = {"Oui", "Non"};
 
-			// trouver l'index de l'employé avec le plus de jours
-			if(malade[i] > malade[max])
-				max = i;
-			
-			// trouver le premier index d'un employé avec un nombre N de jours
-			if(maladeE == 0 && malade[i] == EXACTNB)
-				maladeE = i;
-		}
-		// diviser les moyennes
-		moyenne  /= taille;
-		moyenneP /= taille;
-		
-		// demandé un numéro d'employé et trouvé son index
-		do {
-			nombre = Integer.parseInt(JOptionPane.showInputDialog("Veuillez entrez le numéro d'un employé."));
-			for(i = 0; i < taille; i++)
-				if(numero[i] == nombre) {
-					index = i;
-					i = taille;
-				}
-			
-			if(i == taille)
-				// message d'erreur si le numéro n'existe pas
-				JOptionPane.showMessageDialog(null, "L'employé #"+nombre+" n'est pas dans votre compagnie.\n");
-		}
-		while(i == taille);
-		
-		// afficher les résultats
-		System.out.print("Nombre d'employés ayant accumulé 10 journées de maladie ou plus: "+sommeP+"\n");
-		System.out.print("Moyenne du nombre de journées de maladie: "+moyenne+"\n");
-		System.out.print("Moyenne du nombre de journées de maladie de moins de 10 jours: "+moyenneP+"\n");
-		System.out.print("L'employé "+nom[max]+" (#"+numero[max]+") à le plus de journées de maladie d'accumulées: "+malade[max]+"\n");
-		System.out.print("L'étendue est de "+(malade[max]-malade[min])+"\n");
-		System.out.print("L'employé "+nom[maladeE]+" (#"+numero[maladeE]+") à "+malade[maladeE]+" journées de maladie\n");
-		System.out.print("L'employé "+nom[index]+" (#"+numero[index]+") à "+malade[index]+" journées de maladie\n");
-		
-		// quitter le programme
-		quit(0);
-	}
-	
-	/*
-	 * Si le programme doit se terminer en renvoyant 
-	 * une erreur et sans qu'il plante, cette fonction
-	 * est utilisé.
-	 */
-	private static void quit(int code) {
-		System.exit(code);
-	}
+        int choix;
+
+        //boucler tant et aussi longtemps que l'utilisateur n'a pas quitté
+        session();
+        do {
+            choix = JOptionPane.showOptionDialog(
+                    null, "Voulez-vous quitter?", NOM,
+                    JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null,
+                    menu, menu[0]);
+
+            switch(choix) {
+            case 0:  break;
+            default: session();
+            }
+        } while(choix != 0);
+
+        // quitter le programme
+        quit(0);
+    }
+
+    /*
+     * Cette fonction est utilisé lorsque l'utilisateur
+     * veut regarder les journées de maladie.
+     */
+    public static int session() {
+        final int PSUPNB  = 10; // nombre de journée précis supérieur
+        final int EXACTNB =  8; // nombre de journée précis
+
+        // tableau contenant les informations des employés
+        String nom[] = {"Benoit", "Alice", "Daniel", "Emile", "Julien"};
+        int numero[] = { 20,       10,      40,       50,      80     };
+        int malade[] = { 18,        8,      10,       11,       8     };
+
+        int i;               // contient un compteur
+        int taille;          // contient le nombre d'employé
+        int sommeP      = 0; // contient la somme des journées des employés avec plus de N jours
+        int min         = 0; // contient l'index de l'employé avec le moins de jours
+        int max         = 0; // contient l'index de l'employé avec le plus de jours
+        int maladeE     = 0; // contient l'index de l'employé avec un nombre N de jours
+        int nombre;          // contient un nombre entré par l'utilisateur
+        int index       = 0; // contient l'index de l'employé demandé
+        double moyenne  = 0; // contient la moyenne de jours de tous les employés
+        double moyenneP = 0; // contient la moyenne de jours des employés avec plus de N jours
+
+        String resultat; // contient le message final
+        String input;    // contient la valeur renvoyé par getNumber()
+        DecimalFormat decimal = new DecimalFormat("#0.0");
+
+        for(i = 0, taille = nom.length; i < taille; i++) {
+            // somme des journées des employés avec plus de N jours
+            if(malade[i] >= PSUPNB)
+                sommeP++;
+
+            // somme de la moyenne des journées des employés avec plus de N jours
+            if(malade[i] < PSUPNB)
+                moyenneP += malade[i];
+
+            // somme de la moyenne des journées de tous les employées
+            moyenne += malade[i];
+
+            // trouver l'index de l'employé avec le moins de jours
+            if(malade[i] < malade[min])
+                min = i;
+
+            // trouver l'index de l'employé avec le plus de jours
+            if(malade[i] > malade[max])
+                max = i;
+
+            // trouver le premier index d'un employé avec un nombre N de jours
+            if(maladeE == 0 && malade[i] == EXACTNB)
+                maladeE = i;
+        }
+        // diviser les moyennes
+        moyenne  /= taille;
+        moyenneP /= taille;
+
+        // demandé un numéro d'employé et trouver son index
+        do {
+            input = getNumber("Veuillez entrez le numéro d'un employé.");
+            if(input == ERR) return 1;
+            nombre = Integer.parseInt(input);
+
+            for(i = 0; i < taille; i++)
+                if(numero[i] == nombre) {
+                    index = i;
+                    i = taille;
+                }
+
+            // message d'erreur si le numéro n'existe pas
+            if(i == taille)
+                JOptionPane.showMessageDialog(null, "L'employé #"+nombre+" n'est pas dans votre compagnie.\n");
+        }
+        while(i == taille);
+
+        // afficher les résultats
+        resultat  = "Il y a "+sommeP+" "+(sommeP>1?"employés":"employé")+" ayant accumulé 10 journées de maladie ou plus.\n";
+        resultat += "La moyenne est de "+decimal.format(moyenne)+" "+(moyenne>1?"journées":"journée")+" de la maladie par employé.\n";
+        resultat += "La moyenne est de "+decimal.format(moyenneP)+" "+(moyenneP>1?"journées":"journée")+" de la maladie par employé ayant moins de 10 journées prises.\n";
+        resultat +=  nom[max]+" (#"+numero[max]+") a le plus de journées de maladie avec "+malade[max]+" "+(malade[max]>1?"journées":"journée")+" d'accumulées.\n";
+        resultat += "L'étendue du nombre de journée de maladie est de "+(malade[max]-malade[min])+".\n";
+        resultat +=  nom[maladeE]+" (#"+numero[maladeE]+") a exactment "+malade[maladeE]+" "+(malade[maladeE]>1?"journées":"journée")+" de maladie.\n\n";
+        resultat +=  nom[index]+" (#"+numero[index]+") a "+malade[index]+" "+(malade[index]>1?"journées":"journée")+" de maladie.";
+
+        JOptionPane.showMessageDialog(null, new JTextArea(resultat));
+        return 0;
+    }
+
+    /*
+     * Cette fonction va demandé un nombre entre la borne mininal.
+     * Elle s'assure que le chiffre est valide.
+     */
+    private static String getNumber(String text) {
+        int nombre   = 0;
+        String choix = ""; // contient le texte entré par l'utilisateur
+
+        do {
+            choix = JOptionPane.showInputDialog(text);
+
+            // s'assurer que la réponse entrée est correcte
+            if(choix == null)
+                choix = ERR;
+            else
+                try {
+                    nombre = Integer.parseInt(choix);
+
+                    // le nombre doit être positif
+                    if(nombre >= 0)
+                        choix = null;
+                } catch(NumberFormatException except) {}
+        } while(choix != ERR && choix != null);
+
+        if(choix != ERR)
+            choix = ""+nombre;
+
+        return choix;
+    }
+
+    /*
+     * Si le programme doit se terminer en renvoyant une erreur
+     * et sans qu'il plante, cette fonction est utilisé.
+     */
+    private static void quit(int code) {
+        System.exit(code);
+    }
 }
--- semaine_9/src/semaine_9/Tableau_3.java
+++ semaine_9/src/semaine_9/Tableau_3.java
@@ -1,11 +1,10 @@
 package semaine_9;
 
-import java.text.NumberFormat;
-
 import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
+import java.text.NumberFormat;
 
-/* 
+/*
  * Fichier     : Tableau_3.java
  * Project     : Laboratoires sur les tableaux
  * Objectifs   : Compilation de votes pour une municipalité
@@ -16,110 +15,127 @@ import javax.swing.JTextArea;
  */
 
 public class Tableau_3 {
-	final static int ERR    = -1; // constante du code d'erreur si l'utilisateur annule
-	final static int BUREAU =  5; // constante du nombre de bureau
-	final static String nom[] = {"Monique Lagacé", "Myriam Laposte",  "Julien Lafrenière", 
-	                             "Alex Beaubien",  "Jules Laliberté", "Diana Montplaisir"};
-	
+    final static String NOM   = "Compilateur de votes";
+    final static String ERR   = "Annuler";
+    final static String CAN[] = {"Monique Lagacé", "Myriam Laposte",  "Julien Lafrenière",
+                                 "Alex Beaubien",  "Jules Laliberté", "Diana Montplaisir"};
+    final static int BUREAU   =  5; // constante du nombre de bureau
+
 	public static void main(String[] args) {
-		final String menu[] = {"Quitter", "Nouvelle compilation"};
-		int choix;
-		boolean quit;
-		
-		//boucler tant et aussi longtemps que l'utilisateur n'a pas quitté
-		quit = false;
-		while(!quit) {
-			choix = JOptionPane.showOptionDialog(
-					null, "Quelle action voulez-vous faire?", "Vote",
-					JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, 
-					menu, menu[0]);
-			
-			switch(choix) {
-			case 0:  quit = true; break;
-			default: compilation();
-			}
-		}
-		
-		// quitter le programme
-		quit(0);
-	}
-	
-	private static void compilation()  {
-		int    resul[] = new int[nom.length];
-		double pourc[] = new double[nom.length];
-		
-		NumberFormat percent = NumberFormat.getPercentInstance();
-		
-		String resultat ;
-		int somme;
-		int vote;
-		int gagnant = 0;
-		int i;
-		int j;
-		
-		for(i = somme = 0; i < BUREAU; i++)
-			for(j = 0; j < nom.length; j++) {
-				vote = getVote(i, j);
-				resul[j] += vote;
-				somme    += vote;
-				if(resul[j] == ERR)
-					quit(1);
-			}
-		
-		resultat = "Résultat des élections municipales.\n\n";
-		for(i = 0; i < nom.length; i++) {
-			if(resul[i] > resul[gagnant]) 
-				gagnant = i;
-			
-			pourc[i] = (double)resul[i]/somme;
+        final String menu[] = {"Oui", "Non"};
 
-			resultat += (nom[i]+":     \t"+resul[i]+" "+(resul[i]>1?"votes":"vote")
-			         +" ("+percent.format(pourc[i])+")\n");
-		}
-		resultat += ("\nLe gagnant des élections municipales est "
-		            +nom[gagnant]+" avec "+resul[gagnant]+" "
-		            +(resul[gagnant]>1?"votes":"vote")+".");
-		JOptionPane.showMessageDialog(null, new JTextArea(resultat));
-	}
-	
-	/*
-	 * Cette fonction va demandé un nombre de votes.
-	 * Elle s'assure que le chiffre est valide.
-	 */
-	private static int getVote(int bureau, int index) {
-		String choix = ""; // contient le texte entré par l'utilisateur
-		int vote     = 0;  // contient la note entré par l'utilisateur
+        int choix;
 
-		while(choix != null) {
-			// demander un nombre à l'utilisateur
-			choix = JOptionPane.showInputDialog("Concernant le bureau de scrutin numéro "+(bureau+1)+", "+
-			                                    "le nombre de votes pour "+nom[index]+" est:");
-			
-			// quitter si annuler est cliqué
-			if(choix == null)
-				vote = ERR;
-			else {
-				try {
-					vote = Integer.parseInt(choix);
-					choix = null;
-					
-					// erreur si le nombre est négatif
-					if(vote < 0) 
-						choix = "";
-				// erreur si ce n'est pas un nombre
-				} catch(NumberFormatException except) {}
-			}
-		}
-		
-		return vote;
-	}
-	
-	/*
-	 * Si le programme doit se terminer en renvoyant 
-	 * une erreur et sans qu'il plante, cette fonction
-	 * est utilisé.
-	 */
-	private static void quit(int code) {
-		System.exit(code);
-	}
+        //boucler tant et aussi longtemps que l'utilisateur n'a pas quitté
+        session();
+        do {
+            choix = JOptionPane.showOptionDialog(
+                    null, "Voulez-vous quitter?", NOM,
+                    JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null,
+                    menu, menu[0]);
+
+            switch(choix) {
+            case 0:  break;
+            default: session();
+            }
+        } while(choix != 0);
+
+        // quitter le programme
+        quit(0);
+    }
+
+    /*
+     * Cette fonction est utilisé lorsque l'utilisateur
+     * veut faire compiler des votes.
+     */
+    private static int session()  {
+        int    resul[] = new int[CAN.length];    // tableau contenant les résultats de chaque personne
+        double pourc[] = new double[CAN.length]; // tableau contenant les résultats sous un pourcentage
+
+        int somme;       // contient le nombre total de vote
+        int vote;        // contient le nombre de vote entré par l'utilisateur
+        int gagnant = 0; // contient l'index du gagnant
+        int i;           // contient un compteur
+        int j;           // contient un compteur
+
+        String resultat; // contient la chaîne de charactères contenant les résultats
+        String input;    // contient la valeur renvoyé par getNumber()
+        NumberFormat percent = NumberFormat.getPercentInstance();
+
+        // pour chaque bureaux, demander le nombre de vote pour chaque personnes
+        for(i = somme = 0; i < BUREAU; i++)
+            for(j = 0; j < CAN.length; j++) {
+                // demander le nombre de vote pour X bureau et Y personne
+                // si l'utilisateur annule, le programme se termine
+                input = getVote(i, j);
+                if(input == ERR) return 1;
+                vote = Integer.parseInt(input);
+
+                // classer ces votes et les ajouter à la somme total de vote
+                resul[j] += vote;
+                somme    += vote;
+            }
+
+        // création des résultats
+        resultat = "Résultat des élections municipales.\n\n";
+        for(i = 0; i < CAN.length; i++) {
+            // en même temps, déterminer la gagnant des élections
+            if(resul[i] > resul[gagnant])
+                gagnant = i;
+
+            // en même temps, convertir le nombre de vote en pourcentage
+            pourc[i] = (double)resul[i]/somme;
+
+            // ajouter la ligne d'un député dans le résultat
+            resultat += (CAN[i]+":     \t"+resul[i]+" "+(resul[i]>1?"votes":"vote")
+                     +" ("+percent.format(pourc[i])+")\n");
+        }
+
+        // finaliser et afficher les résultats avec le gagnant
+        resultat += ("\nLe gagnant des élections municipales est "
+                    +CAN[gagnant]+" avec "+resul[gagnant]+" "
+                    +(resul[gagnant]>1?"votes":"vote")+".");
+
+        JOptionPane.showMessageDialog(null, new JTextArea(resultat));
+        return 0;
+    }
+
+    /*
+    * Cette fonction va demandé une nombre de vote entre la borne mininal
+    * Elle s'assure que le chiffre est valide.
+    */
+    private static String getVote(int bureau, int index) {
+        int nombre   = 0;
+        String choix = ""; // contient le texte entré par l'utilisateur
+
+        do {
+            choix = JOptionPane.showInputDialog("Concernant le bureau de scrutin numéro "+(bureau+1)+", "+
+                                                "le nombre de votes pour "+CAN[index]+" est:");
+
+            // s'assurer que la réponse entrée est correcte
+            if(choix == null)
+                choix = ERR;
+            else
+                try {
+                    nombre = Integer.parseInt(choix);
+
+                    // le nombre doit être positif
+                    if(nombre >= 0)
+                        choix = null;
+                } catch(NumberFormatException except) {}
+        } while(choix != ERR && choix != null);
+
+        if(choix != ERR)
+            choix = ""+nombre;
+
+        return choix;
+    }
+
+    /*
+    * Si le programme doit se terminer en renvoyant une erreur
+    * et sans qu'il plante, cette fonction est utilisé.
+    */
+    private static void quit(int code) {
+        System.exit(code);
+    }
 }
